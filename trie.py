8856c8ef08f60954cfa9fe72a585e556b3b34f97
shantanu <shaantanutripathi@gmail.com>
2021-09-11 14:46:42 +0530

    Delete in Trie

--- trie.py
+++ trie.py
@@ -4,8 +4,9 @@ class TrieNode:
     def __init__(self): 
         self.data = '/'
         self.children = [None]*SIZE
-        self.count = 0
-        self.isEndOfWord = False
+        self.count = 0  # For delete operation to work out
+        self.numEndofWord = 0  # To find Occurance of String
+        self.isEndOfWord = False # To find Existance of String
 class Trie:
     # base for binary (0 - 1) -> str = "0" and alphabet (a - z) -> str = "a"
     def __init__(self, base):
@@ -20,15 +21,18 @@ class Trie:
             # If Already Present, Just Crawl Down.
             if(matches and at.children[self.Ord(i)]):
                 at = at.children[self.Ord(i)]
+                at.count += 1
             # Else Create a new TrieNode and Mark data as i and matches False
             else:
                 at.children[self.Ord(i)] = TrieNode()
                 at = at.children[self.Ord(i)]
+                at.count += 1
                 at.data = i
                 matches = False
+        # Seperate work for last one
         at.isEndOfWord = True
-        # Count of end of string is increased by 1
-        at.count += 1
+        ## Count of end of string is increased by 1
+        at.numEndofWord += 1
     def search(self,string):
         at = self.root
         matches = True
@@ -52,7 +56,7 @@ class Trie:
         if(at != start):
             curr += at.data
         if(at.isEndOfWord == True):
-            print(curr, at.count)
+            print(curr, at.numEndofWord)
         for i in range(SIZE):
             if(at.children[i]):
                 self.content(at.children[i], start, curr)
@@ -68,15 +72,34 @@ class Trie:
                 break
         if(matches == True):
             self.content(at, at)
+    # Also prints count of each word along with whole string occurance
+    def contentCount(self, at, start, curr = ""):
+        if(at != start):
+            curr += at.data
+        print(at.data, at.count)
+        if(at.isEndOfWord == True):
+            print(curr, "->", at.numEndofWord)
+        for i in range(SIZE):
+            if(at.children[i]):
+                self.contentCount(at.children[i], start, curr)
+    # Assumes that string is present in trie
+    def delete(self, string):
+        at = self.root
+        for i in string:
+            at = at.children[self.Ord(i)]
+            at.count -= 1
+        at.numEndofWord -= 1
+        at = self.root
+        for i in string:
+            if(at.children[self.Ord(i)].count == 0):
+                at.children[self.Ord(i)] = None
+                break
+            at = at.children[self.Ord(i)]
 T = Trie("a")
-keys = ["the","a","there","answer","there","any","by","their"]
+keys = ["the","a","there","answer","there","any","by","their","anyway", "ask", "afk","ask"]
 for key in keys:
     T.insert(key)
 # print(T.search("ther"))
-T.content(T.root, T.root)
-T.autoComplete("the")
-           
-
-
-
-    
+T.contentCount(T.root, T.root)
+T.delete("there")
+T.contentCount(T.root, T.root)
